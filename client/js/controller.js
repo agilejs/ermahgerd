c744676ab91792c4ff98d0db99ac2273c85bd7d4
ccworkshop <social@cihadkirhan.com>
2013-10-08 14:14:34 +0200

    Added form validation

--- client/js/controller.js
+++ client/js/controller.js
@@ -34,6 +34,9 @@ function MoviesAddCtrl ($scope, $http, $location) {
             $location.path('/movies/' + res.id);
         });
     };
+    $scope.validate = function (movie) {
+        console.log("Title: " + movie.title);
+    };
 }
 
 function MovieDetailCtrl ($scope, $http, $location, moviesResponse) {
